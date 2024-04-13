faec6d13c6ef612d6fdae283e5fe57df7e26a45f
LiamDeSaldanha <129397980+LiamDeSaldanha@users.noreply.github.com>
2024-04-13 14:22:43 +0200

    nde

--- Node.java
+++ Node.java
@@ -4,16 +4,20 @@ public class Node {
 
      private int x, y;
      private String nodeNum;
-     private String herb;
-     private boolean covered;
+     private Herb herb;
+     private double waterCoverage;
+     private double sunCoverage;
+     private String type;
+
      public String[] edges = new String[10000];
 
-
-     public Node(String nodeNum, int x, int y, String herb) {
+     public Node(String nodeNum, int x, int y, Herb herb) {
           this.nodeNum = nodeNum;
           this.x = x;
           this.y = y;
           this.herb = herb;
+          this.covered = 0;
+          type = "Empty";
      }
 
      public void setHerb(String herbName) {
@@ -25,4 +29,35 @@ public class Node {
           this.edges = edges;
      }
 
+     public void setWaterCoverage(double water) {
+          this.waterCoverage = water;
+     }
+
+     public void setSunCoverage(double water) {
+          this.sunCoverage = water;
+     }
+
+     public String getName() {
+          return nodeNum;
+     }
+
+     public String getType() {
+          return type;
+     }
+
+     public void setType(String type) {
+          this.type = type;
+     }
+
+     public void getHerb() {
+          return herb;
+     }
+
+     public int getX() {
+          return x;
+     }
+
+     public int getY() {
+          return y;
+     }
 }
